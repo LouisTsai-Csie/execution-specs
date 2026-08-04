5002ad513c5553275dc724bd3339f84063260505
LouisTsai <q1030176@gmail.com>
2026-08-04 18:25:48 +0800

    refactor: enhance test implementation

--- tests/benchmark/stateful/bloatnet/test_account_query.py
+++ tests/benchmark/stateful/bloatnet/test_account_query.py
@@ -209,6 +209,7 @@ def test_account_access(
             opcode(
                 address=address_source.address_op(),
                 value=value_sent,
+                gas=Op.GAS,
                 # Gas accounting
                 address_warm=access_warm,
                 value_transfer=value_sent > 0,
